a06c83c52c86d8e59ab4cbfb1be5c9c9f7c34755
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-02-24 18:11:52 -0500

    actualizando cambios en la pagina principal del sistema 1.1

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+/* recuperando el boton  */
+let boton = document.getElementById("boton");
+
+/* recuperando el menu de navegacion */
+let nav = document.getElementById("nav");
+
+/* creando un funcion */
+function mostrar_menu (){
+    nav.classList.toggle("mostrarMenu");
+}
+
+boton.addEventListener("click", mostrar_menu)
